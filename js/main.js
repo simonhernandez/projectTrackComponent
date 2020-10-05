8b5f8476fd9f67a40d8a11db055301190b5d9aa7
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-04 21:19:44 -0400

    Add inital js code

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Constants
+const navToggle = document.getElementById('navToggle');
+const menu = document.getElementById('menu');
+
+navToggle.addEventListener('click', () => {
+    alert('test');
+})
