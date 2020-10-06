304f50ac0887b594e5aa58aef67737138285c874
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-06 03:42:19 -0400

    Add toggle code for navtoggle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 // Constants
 const navToggle = document.getElementById('navToggle');
 const menu = document.getElementById('menu');
+const icon = document.getElementById('icon');
 
 navToggle.addEventListener('click', () => {
+    
     menu.classList.toggle('show');
+
+    if (icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else if (icon.classList.contains('fa-times')){
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 })
