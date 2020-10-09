77f14f190f24660c081c53960b54566346bb31fc
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-10-08 21:19:08 -0400

    Add toggle func

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 // Constants
 const navToggle = document.getElementById('navToggle');
 const menu = document.getElementById('menu');
-const icon = document.getElementById('icon');
+var toggle = false;
+
+function toggleImg(){
+    
+    if (toggle){
+        navToggle.style.backgroundImage = "url(images/icon-hamburger.svg)"
+    } else {
+        navToggle.style.backgroundImage = "url(images/icon-close.svg)"
+    }
+
+    toggle = !toggle;
+}
 
 navToggle.addEventListener('click', () => {
     
     menu.classList.toggle('show');
 
-    if (icon.classList.contains('fa-bars')){
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-times');
-    } else if (icon.classList.contains('fa-times')){
-        icon.classList.remove('fa-times');
-        icon.classList.add('fa-bars');
-    }
+    toggleImg();
 })
